42ce0fc5d0e579bbd674516bd1c33d2135ea2164
Strange-can <179990912+Strange-can@users.noreply.github.com>
2024-11-17 22:41:03 +0800

    Change content in #content div to be created by js
    All child elements in #content div is now entirely created by javascript.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import "./style.css"
 import pizzaPic from "./pizza-odin.jpg"
-import { hi } from "./test.js"
 
-console.log(hi)
+//parent container
 const content = document.getElementById("content")
-const homeImage = document.createElement('img')
+
+//children elements
+const brand = document.createElement("p")
+brand.setAttribute("id", "brand")
+brand.textContent = 'PIZZA PLACE'
+content.appendChild(brand)
+
+const saleLine = document.createElement("p")
+saleLine.setAttribute("id", "sale-line")
+saleLine.textContent = 'Pizzas that are baked on order! (YUM!)'
+content.appendChild(saleLine)
+
+const homeImage = document.createElement("img")
 homeImage.setAttribute("id", "home-img")
 homeImage.src = pizzaPic
 content.appendChild(homeImage)
