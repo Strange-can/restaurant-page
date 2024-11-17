eaff74a1fb55d0e22f37ff939cb61e48801acd04
Strange-can <179990912+Strange-can@users.noreply.github.com>
2024-11-17 20:37:27 +0800

    Add designs for home page
    Home page has been created, yet to replace all html code in content section to js code.
    Design for header section not done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import "./style.css"
+import pizzaPic from "./pizza-odin.jpg"
 import { hi } from "./test.js"
 
 console.log(hi)
+const content = document.getElementById("content")
+const homeImage = document.createElement('img')
+homeImage.setAttribute("id", "home-img")
+homeImage.src = pizzaPic
+content.appendChild(homeImage)
+
+const imgCredit = document.createElement("p")
+imgCredit.textContent = 'Image made by freepik'
+imgCredit.setAttribute("id", "img-credit")
+content.appendChild(imgCredit)
